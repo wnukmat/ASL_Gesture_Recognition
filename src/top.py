df35eb096f261ad9cb48ae952b61be49b361a992
Mansur Amin <maamin@ucsd.edu>
2018-05-02 16:59:07 -0700

    added images and top leve file

---
+++ src/top.py
@@ -0,0 +1,12 @@
+################################################################################
+# Title: top level file for ASL net 
+#
+# Author: Mansur Amin
+# 
+# Description: Top leve file for ASL NN. 
+# 
+################################################################################
+
+# libraries
+import numpy as np
+
