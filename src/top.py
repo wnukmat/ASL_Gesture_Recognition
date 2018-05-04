41c7ff02a8382ba64b9472dae34abd4ddc7ad9ad
Mansur Amin <maamin@ucsd.edu>
2018-05-04 14:34:18 -0700

    checking in top file changes

--- src/top.py
+++ src/top.py
@@ -9,4 +9,19 @@
 
 # libraries
 import numpy as np
+import os 
+
+path = 'C:\\Users\\maamin\\Desktop\\ECE_285_ML_DL\\ASL_Gesture_Recognition\\data\\sign_mnist_train.csv'
+
+num_lines = sum(1 for line in open(path))
+
+
+myfile = open(path,'r')
+j = 0
+read_lines = np.zeros((num_lines,785))
+for line in myfile :
+    if(j == 1) :
+        read_lines[j,:] = line.split(',')
+        j = j+1
+myfile.close()
 
