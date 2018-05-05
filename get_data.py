660e8fd7ec68210481165c47fc2f836f912e5145
wnukmat <35384379+wnukmat@users.noreply.github.com>
2018-05-04 17:07:09 -0700

    Updated to allow download of only specific words!
    Example command prompt to download three specific words
    
    python get_data.py twenty along lonely

--- get_data.py
+++ get_data.py
@@ -8,7 +8,9 @@ def get_vid(url, word, num):
 	with open('data/' + word + '/' + str(num) + '.mov','wb') as output:
 		output.write(u.read())
 	
-def main():
+def main(Target_Word=None):
+	if(Target_Word!=None):
+		found_word = False
 	try:											#check for data directory and create if needed
 		os.stat('data/')
 	except:
@@ -18,23 +20,57 @@ def main():
 	ws = wb.get_sheet_by_name('Sheet1')
 
 	for i in range(3,13236):
-		word = ws.cell(row=i, column=2).value		#check for new word
-		
-		if(word == ' '):							#disregard variations of words
-			pass
-		elif(word != None):							#if new word
-			word = word.replace("/", "-")
-			dir = word
-			example = 1								#set to first example of word
-			try:
-				os.stat('data/' + dir)				#check for directory associated with word
-			except:
-				os.mkdir('data/' + dir) 			#create if needed
-		else:
-			url = str(ws.cell(row=i, column=12).value[12:-9])	#get hyperlink
-			get_vid(url, dir, example)				#download video
-			example += 1							#increment to next example of word
+		if(Target_Word != None):						#Download Specific Word
+			word = ws.cell(row=i, column=2).value		#check for word
+			if(word == ' '):							#disregard variations of words
+				pass				
+			elif(word != None):							#if new word
+				if(word.lower() == Target_Word.lower()):
+					word = word.replace("/", "-")
+					dir = word
+					example = 1								#set to first example of word
+					try:
+						os.stat('data/' + dir)				#check for directory associated with word
+					except:
+						os.mkdir('data/' + dir) 			#create if needed
+					found_word = True
+				else:
+					if(found_word == True):
+						print '...downloaded!'
+						break
+			else:
+				if(found_word == True):
+					url = str(ws.cell(row=i, column=12).value[12:-9])	#get hyperlink
+					get_vid(url, dir, example)				#download video
+					example += 1							#increment to next example of word				
+		else:											#Download Entire Dataset
+			word = ws.cell(row=i, column=2).value		#check for new word
+				
+			if(word == ' '):							#disregard variations of words
+				pass
+			elif(word != None):							#if new word
+				word = word.replace("/", "-")
+				dir = word
+				example = 1								#set to first example of word
+				try:
+					os.stat('data/' + dir)				#check for directory associated with word
+				except:
+					os.mkdir('data/' + dir) 			#create if needed
+			else:
+				url = str(ws.cell(row=i, column=12).value[12:-9])	#get hyperlink
+				get_vid(url, dir, example)				#download video
+				example += 1							#increment to next example of word
 			
 
 if __name__ == "__main__":
-	main()
+	if(len(sys.argv) > 1):
+		for i in range(1,len(sys.argv)):
+			print 'Downloading: ', sys.argv[i], 
+			main(sys.argv[i])
+	else:
+		print 'Downloading Complete Dataset'
+		main()
+	
+	
+	
+	
