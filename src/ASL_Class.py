c30a2e93005697c3d9120d0b54bc031f4203b6d1
Mansur Amin <mansuramin@Mansurs-MacBook-Pro.local>
2018-05-05 00:10:26 -0700

    Added a class for all functions. Working top file, able to read data and visualize it.

---
+++ src/ASL_Class.py
@@ -0,0 +1,46 @@
+################################################################################
+# Title: ASL_Class.py
+#
+# Author: Mansur Amin
+#
+# Description: functions to be used in the ASL training and testing phase
+#
+# Current Status: Contains all functions needed for program to run
+################################################################################
+
+#####################################################################
+# libraries
+#####################################################################
+import numpy as np
+import matplotlib.pyplot as plt
+
+#####################################################################
+# def read_data(path) :
+# Reads data specidfed by path and stores into numpy array, Splits data nad lbl
+# path = location of where data is stored
+#####################################################################
+def read_data(path) :
+   num_lines = sum(1 for line in open(path)) # get number of lines in file
+   myfile = open(path,'r')
+   j = 0
+   read_lines = np.zeros((num_lines,785))    # expects a 25x25 pixel row vector
+   for line in myfile :
+      if(j >= 1) :
+         read_lines[j,:] = np.asarray(line.split(','))
+      j = j+1
+   myfile.close()
+   lables = read_lines[:,0]
+   read_lines = np.delete(read_lines,0,1)
+   return read_lines,lables.astype(np.int16) # .reshape(len(lables),1)
+
+#####################################################################
+# def vis_data(dat_vec,dat_lbl) :
+# Visualize data.
+# dat_vec = vector representing data
+# dat_lbl = label representing data
+#####################################################################
+def vis_data(dat_vec,dat_lbl) :
+   lbl_map = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+   dat_mat = dat_vec.reshape(28,28)
+   plt.title('Image Label = ' + str(dat_lbl) + ' = ' + lbl_map[dat_lbl])
+   plt.imshow(dat_mat)
--- src/top.py
+++ src/top.py
@@ -1,27 +1,41 @@
 ################################################################################
-# Title: top level file for ASL net 
+# Title: top level file for ASL net
 #
 # Author: Mansur Amin
-# 
-# Description: Top leve file for ASL NN. 
-# 
+#
+# Description: Top leve file for ASL NN.
+#
+# Current Status: Calls all functions and imports all files needed to run
+# training net.
 ################################################################################
 
 # libraries
 import numpy as np
-import os 
+import matplotlib.pyplot as plt
+import ASL_Class as asl
 
-path = 'C:\\Users\\maamin\\Desktop\\ECE_285_ML_DL\\ASL_Gesture_Recognition\\data\\sign_mnist_train.csv'
+#####################################################################
+# Main Program
+#####################################################################
 
-num_lines = sum(1 for line in open(path))
+####### Read training data
+
+# Windows Path
+#path = 'C:\\Users\\maamin\\Desktop\\ECE_285_ML_DL\\ASL_Gesture_Recognition\\data\\sign_mnist_train.csv'
+
+# Mac path
+path = '/Users/mansuramin/Desktop/ECE_285_ML/data/sign_mnist_train.csv'
+
+trn_dat_vec,trn_lbl = asl.read_data(path)
+indx = 10240
+asl.vis_data(trn_dat_vec[indx,:],trn_lbl[indx])
 
 
-myfile = open(path,'r')
-j = 0
-read_lines = np.zeros((num_lines,785))
-for line in myfile :
-    if(j == 1) :
-        read_lines[j,:] = line.split(',')
-        j = j+1
-myfile.close()
+
+
+
+
+
+
+
 
